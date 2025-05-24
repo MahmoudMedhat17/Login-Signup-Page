81c86b7f9d4a95cf24e1518539d35567cb68b1fc
Mahmoud Medhat <M7moooud.17@gmail.com>
2025-05-24 04:20:25 +0300

    Finished Login and Sign up page with firebase authentication.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,14 @@ const Home = () => {
 
   return (
     <div className="flex justify-center items-center w-full h-screen">
-        <h3 className="text-2xl font-semibold text-center">Welcome back {currentUser?.email ? currentUser.email : "User"}! :)</h3>
+      {
+        currentUser?.email ?
+        (
+        <h3 className="text-2xl font-semibold text-center">Welcome back {currentUser?.email && currentUser.email}</h3>
+        )
+        :
+        <h3 className="text-2xl font-semibold text-center">Welcome back!</h3>
+      }
     </div>
   )
 }
